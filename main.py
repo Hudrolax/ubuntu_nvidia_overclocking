19eb478c0ff4bf37b50fcb7473366ef2ffc82868
Hudrolax <hudro795@gmail.com>
2020-08-23 18:47:54 +0300

    some changes

--- main.py
+++ main.py
@@ -4,23 +4,35 @@ import threading
 
 class Overclocking:
     def __init__(self):
-        self.powerlimit_watts = 152
+        self._powerlimit_watts = 152
+        self._funs_speed = 100
         # looking for Nvidia cards
         self.nvidia_cards = int(subprocess.check_output('nvidia-smi -L | wc -l', shell=True).decode())
         print(f'{self.nvidia_cards} NVIDIA cards found.')
 
     def set_powerlimit(self):
-        for card in range(0, 12):
-            _thread = threading.Thread(target=self._threaded_set_powerlimit, args=(card,), daemon=False)
+        for gpu in range(0, self.nvidia_cards):
+            _thread = threading.Thread(target=self._threaded_set_powerlimit, args=(gpu,), daemon=False)
             _thread.start()
 
-    def _threaded_set_powerlimit(self, card):
-        print(f'Start to set powerlimit for card {card}')
-        out = subprocess.check_output(f'sudo nvidia-smi -i {card} -pm 1', shell=True).decode()
+    def _threaded_set_powerlimit(self, gpu):
+        print(f'Start to set powerlimit for card {gpu}')
+        out = subprocess.check_output(f'sudo nvidia-smi -i {gpu} -pm 1', shell=True).decode()
         print(out)
-        out = subprocess.check_output(f'sudo nvidia-smi -i {card} -pl {self.powerlimit_watts}', shell=True).decode()
+        out = subprocess.check_output(f'sudo nvidia-smi -i {gpu} -pl {self._powerlimit_watts}', shell=True).decode()
+        print(out)
+
+    def set_funs_speed(self):
+        for gpu in range(0, self.nvidia_cards):
+            _thread = threading.Thread(target=self._threaded_set_funs_speed, args=(gpu,), daemon=False)
+            _thread.start()
+
+    def _threaded_set_funs_speed(self, gpu):
+        print(f'Start to set fun speed for card {gpu}')
+        out = subprocess.check_output(f'sudo nvidia-settings -a "[gpu:{gpu}]/GPUFanControlState=1" && sudo nvidia-settings -a "[fan:{gpu}]/GPUTargetFanSpeed={self._funs_speed}"', shell=True).decode()
         print(out)
 
 if __name__ == '__main__':
     obj = Overclocking()
     obj.set_powerlimit()
+    obj.set_funs_speed()
