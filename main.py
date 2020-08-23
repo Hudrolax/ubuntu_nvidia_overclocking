77a4be6538341410f638e53a8734cb833aa0c271
Hudrolax <hudro795@gmail.com>
2020-08-23 19:05:47 +0300

    some changes

--- main.py
+++ main.py
@@ -16,29 +16,27 @@ class Overclocking:
         for gpu in range(0, self.nvidia_cards):
             print(f'Start to set powerlimit for card {gpu}')
             out = subprocess.check_output(f'sudo nvidia-smi -i {gpu} -pm 1', shell=True).decode()
-            print(out)
             out = subprocess.check_output(f'sudo nvidia-smi -i {gpu} -pl {self._POWERLIMIT_WATTS}', shell=True).decode()
-            print(out)
+            print(f'Powerlimit for GPU{gpu} setted to {self._POWERLIMIT_WATTS} W')
 
     def set_funs_speed(self):
         for gpu in range(0, self.nvidia_cards):
             print(f'Start to set fun speed for card {gpu}')
             out = subprocess.check_output(f'nvidia-settings -a "[gpu:{gpu}]/GPUFanControlState=1"', shell=True).decode()
-            print(out)
-            out = subprocess.check_output(f'nvidia-settings -a "[fan:{gpu}]/GPUTargetFanSpeed={self._FUNS_SPEED}"',
-                                          shell=True).decode()
-            print(out)
+            out = subprocess.check_output(f'nvidia-settings -a "[fan:{gpu}]/GPUTargetFanSpeed={self._FUNS_SPEED}"', shell=True).decode()
+            print(f'Fun speed for GPU{gpu} setted to {self._FUNS_SPEED}%')
 
     def set_overclocking(self):
         for gpu in range(0, self.nvidia_cards):
             print(f'Overclocking card {gpu}...')
             out = subprocess.check_output(f'nvidia-settings -a "[gpu:{gpu}]/GPUMemoryTransferRateOffset[1]={self._MEM_CLOCK_OFFSET*2}', shell=True).decode()
-            print(out)
+            print(f'Memory clock offset for GPU{gpu} set to {self._MEM_CLOCK_OFFSET}')
             out = subprocess.check_output(f'nvidia-settings -a "[gpu:{gpu}]/GPUGraphicsClockOffset[1]={self._CLOCK_OFFSET}"', shell=True).decode()
-            print(out)
+            print(f'Core clock offset for GPU{gpu} set to {self._CLOCK_OFFSET}')
 
 if __name__ == '__main__':
     obj = Overclocking()
     obj.set_powerlimit()
     obj.set_funs_speed()
     obj.set_overclocking()
+    print('Done!')
